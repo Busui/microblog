ea0b524b997d4b1a17025e94a9cb8310ccac43e8
lovekernel <busui@tp.edu.tw>
2018-09-19 23:54:58 +0800

    The first time I use the jinja2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route("/index")
 @app.route("/")
 def index():
-    return "Hello Flask."
+    user = {"username": "lovekernel"}
+    return render_template("index.html", title = "Home page", user = user)
