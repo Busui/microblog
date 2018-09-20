9fbe4e832c55e2bfe38e5bb63d63f27ae7151762
lovekernel <busui@tp.edu.tw>
2018-09-20 17:59:28 +0800

    fix some writing mistakes

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, get_flashed_messages
 from app import app
 from app.forms import LoginForm
 
@@ -20,12 +20,21 @@ def index():
 
 
 
-@app.route('/login')
+# @app.route('/login', methods = ['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         flash('Login request for user {}, remember_me={}'.format(
+#             form.username.data, form.remember_me.data
+#         ))
+#         return redirect(url_for('index'))
+#     return render_template('login.html', title="Sign In", form = form)
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login request for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data
-        ))
-        return redirect(url_for('index'))
-    return render_template('login.html', title="Sign In", form = form)
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
