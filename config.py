5a2f2ee0aacd53ce07818499aec845893b8427be
lovekernel <busui@tp.edu.tw>
2018-09-21 18:04:16 +0800

    added blank line

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "Just some I don't know."
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
