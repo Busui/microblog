f1af1297401d7776b3d9379cb99262f4258437b3
lovekernel <busui@tp.edu.tw>
2018-09-30 23:05:49 +0800

    add login_message

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,7 @@ migrate = Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 
 mail = Mail(app)
 bootstrap = Bootstrap(app)
