3871c62814520abb02b09f7ed8683507399c22ce
lovekernel <busui@tp.edu.tw>
2018-10-06 12:22:26 +0800

    fix spelling mistake

--- app/main/forms.py
+++ app/main/forms.py
@@ -39,4 +39,3 @@ class SearchForm(FlaskForm):
         if 'csrf_enabled' not in kwargs:
             kwargs['csrf_enabled'] = False
         super(SearchForm, self).__init__(*args, **kwargs)
-        
