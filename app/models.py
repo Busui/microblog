9c344adf6f0c0d51d58a291e94755b76d67a5cd2
lovekernel <busui@tp.edu.tw>
2018-10-14 11:08:50 +0800

    add notification table in databases

--- app/models.py
+++ app/models.py
@@ -165,11 +165,11 @@ class Message(db.Model):
 
 
 class Notification(db.Model):
-    id = db.Column(dn.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), index=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     timestamp = db.Column(db.Float, index=True, default=time)
-    payload_json = db.Column(db.text)
+    payload_json = db.Column(db.Text)
 
     def get_data(self):
         return json.loads(str(self.payload_json))
